67227511744b2acf9eadab9b07576c2de57485d9
TunmiDev <adewoleoluwatunmise@gmail.com>
2024-11-22 00:17:16 +0100

    updated the jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Weather from "./Components/Weather";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="container"></div>
+      <Weather />
     </div>
   );
 };
