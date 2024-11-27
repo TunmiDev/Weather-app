a0f753404863342a969e654aa3b09936e9a1cb8a
TunmiDev <adewoleoluwatunmise@gmail.com>
2024-11-27 23:54:04 +0100

    added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ const App = () => {
         <Weather defaultCity=" Lagos" />
         <footer>
           This project was coded by {""}
-          <a href="" target="_blank">
+          <a href="https://github.com/TunmiDev" target="_blank">
             TunmiDev
           </a>
           {""} and is {""}
-          <a href="" target="_blank">
+          <a href="https://github.com/TunmiDev/Weather-app" target="_blank">
             open-sourced on Github
-          </a>
+          </a>{" "}
+          and {""}
+          {""}
+          <a href="https://weather-app-9vzt.onrender.com">hosted on Netlify.</a>
         </footer>
       </div>
     </div>
